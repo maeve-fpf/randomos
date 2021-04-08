dad324c2ce0c6f07f2af2452470cc49bce1e97fc
Kushal Das <mail@kushaldas.in>
2021-04-08 19:29:24 +0530

    Adds add_numbers example

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,26 @@
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
+/// Says hello to the name given. Returns a string.
 #[pyfunction]
-fn hello(name: String) -> PyResult<String>{
+#[text_signature = "(name)"]
+fn hello(name: String) -> PyResult<String> {
     let answer = format!("Hello {}", name);
     Ok(answer)
 }
 
-/// A Python module implemented in Rust.
+/// Adds two numbers and returns the result
+#[pyfunction]
+#[text_signature = "(a, b)"]
+fn add_numbers(a: i64, b: i64) -> PyResult<i64> {
+    Ok(a + b)
+}
+
+/// A Python module implemented in Rust with random OS things.
 #[pymodule]
 fn randomos(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction! (hello, m)?)?;
+    m.add_function(wrap_pyfunction!(hello, m)?)?;
+    m.add_function(wrap_pyfunction!(add_numbers, m)?)?;
 
     Ok(())
 }
-
