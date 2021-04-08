8192d8b03b867304ae54bd93a423cf839ce32381
Maeve Andrews <maeve@freedom.press>
2021-04-08 13:18:48 -0400

    try reading unicode

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use pyo3::create_exception;
 use pyo3::exceptions::*;
 use pyo3::prelude::*;
-use pyo3::types::PyBytes;
+use pyo3::types::PyUnicode;
 use pyo3::wrap_pyfunction;
 use std::collections::HashMap;
 use std::fs::File;
@@ -40,9 +40,10 @@ fn add_numbers(a: i64, b: i64) -> PyResult<i64> {
 #[text_signature = "(filename)"]
 fn read_file(py: Python, filename: String) -> PyResult<PyObject> {
     let mut file = File::open(filename).expect("no file found");
-    let mut result = Vec::new();
-    file.read_to_end(&mut result).unwrap();
-    let res = PyBytes::new(py, &result);
+    let mut buf = Vec::new();
+    file.read_to_end(&mut buf).unwrap();
+    let result = std::str::from_utf8(&buf).unwrap();
+    let res = PyUnicode::new(py, &result);
     Ok(res.into())
 }
 
